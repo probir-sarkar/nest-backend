1d3c212823bfc16794bf81762c4830ed0f6df575
probir-kubuntu <probirsarkar855@gmail.com>
2024-10-06 14:03:44 +0530

    all cors add

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
     .setTitle('The Tools API')

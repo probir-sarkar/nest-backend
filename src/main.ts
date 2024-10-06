9f21b99fddb235276ca3b4fe01f4b0c27f2ce85f
probir-kubuntu <probirsarkar855@gmail.com>
2024-10-06 14:12:14 +0530

    port change

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
-  await app.listen(8080);
+  await app.listen(3000);
 }
 bootstrap();

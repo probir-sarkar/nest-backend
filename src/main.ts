8d85976f5b3bdb1f1597d7262a63fac1aba3de54
probir-kubuntu <probirsarkar855@gmail.com>
2024-10-06 14:06:00 +0530

    cors update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://mini-apps.probir.dev'],
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
